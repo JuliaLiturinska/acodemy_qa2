53e28f6457859cf2e2bb650954e05744650f320c
J.Liturinska <julee.ginger@gmail.com>
2022-01-26 18:57:02 +0200

    [feature/java 4]: Homework progress add

---
+++ src/main/java/homework/Example.java
@@ -0,0 +1,14 @@
+package homework;
+
+
+public class Example {
+
+    public static void main(String[] args) {
+        Animal jojo = new Animal("Jojo");
+        jojo.feed();
+        jojo.walk();
+        jojo.feed();
+
+
+    }
+}
