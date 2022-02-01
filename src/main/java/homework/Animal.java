5140508ff0e518a46ce6f8f397197233daf38c03
J.Liturinska <julee.ginger@gmail.com>
2022-02-01 23:24:11 +0200

    [feature/java 4]: Homework progress add

--- src/main/java/homework/Animal.java
+++ src/main/java/homework/Animal.java
@@ -1,21 +1,60 @@
 package homework;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.SQLOutput;
+
+@Slf4j
 public class Animal {
     private String name;
     private  int energyCount = 4;
     private boolean isSleeping = false;
+    private boolean isEating = false;
+    private boolean isPlaying = false;
+
+    public Animal(String name, boolean b2, boolean b1, boolean b, int i) {
+        this.name = name;
+        this.isSleeping = isSleeping;
+        this.isEating = isEating;
+        this.isPlaying = isPlaying;
+        this.energyCount = energyCount;
+
+    }
 
     public void walk() {
-        if(energyCount ==0) {
+        log.info("test log");
+        if (energyCount == 0) {
             System.out.println("Please feed me!");
+        } else if (energyCount > 1) {
+            System.out.println(name + " is happy to go for a walk");
         }
     }
     public void feed() {
-        if(energyCount ==4) {
+        log.info("test log");
+        if (energyCount == 4) {
             System.out.println("No need to feed me!");
-        } else {
-            energyCount
+        } else if (energyCount < 4) {
+            energyCount = 4;
+            System.out.println(name + " is happy, energy now is " + energyCount);
         }
+    }
+    public void play() {
+        if (energyCount ==0) {
+            System.out.println("Please let me sleep!");
+        } else if (energyCount > 1) {
+            System.out.println(name + " is happy to play");
+
+        }
+
+    }
+
+   // public void status() {
+   //     System.out.println("Status of animal", name, isSleeping);
+    // }
+
+    public void goSleep() {
+        if(energyCount == 0);
+        System.out.println(name + " is going to sleep!");
     }
 }
 /*
---
+++ src/main/java/homework/ObjectsHW.java
@@ -0,0 +1,22 @@
+package homework;
+
+public class ObjectsHW {
+
+    public static void main(String[] args) {
+
+        Animal jojo = new Animal("Jojo", false, false, false, 4);
+        Animal arni = new Animal("Arni", false, false, false, 4);
+        Animal jery = new Animal("Jery", false, false, false, 4);
+        Animal cat = new Animal("Cat", false, false, false, 4);
+        Animal dog = new Animal("Doggy", false, false, false, 4);
+        Animal parrot = new Animal("Parrot", false, false, false, 4);
+
+        jojo.feed();
+        jojo.play();
+
+
+
+
+
+    }
+}
